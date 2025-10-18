685878d158d9a5bf2b25ca9030c4198b09815127
xiaochengou123 <xiaocheng4563@qq.com>
2025-10-18 17:41:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ window.seckill = [
 //======================================商品列表 · 结束区============================================
 
 //根据路径判断是否渲染
-if (window.location.pathname.includes('/index.html')) {
+if (window.location.pathname.includes('index.html')) {
   //获取本地存储数据
   let user = localStorage.getItem('currentUser');
   /* 顶部菜单栏 */
@@ -1059,5 +1059,6 @@ if (window.location.pathname.includes('/index.html')) {
     audio.play();//自动播放
   }
 }
+
 
 
